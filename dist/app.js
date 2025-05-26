fa2784b6fff309276ff4bd6747b44d33e46bc133
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2025-05-26 12:54:35 +0600

    added

--- dist/app.js
+++ dist/app.js
@@ -13,7 +13,7 @@ const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
+app.use((0, cors_1.default)({ origin: "https://medimart-client-seven.vercel.app", credentials: true }));
 app.use((0, cookie_parser_1.default)());
 app.use('/api', routes_1.default);
 app.get("/", (req, res) => {
